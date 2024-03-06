031176246a5fa233201f3067016638fff032552b
YangMinJu <yangju12388@gmail.com>
2024-03-06 22:54:33 +0900

    다음 주소api를 사용하여 회원 가입페이지에서 작성할수있도록 추가

--- ArtConnect_Advanced/src/main/webapp/resources/js/member/memberJoin.js
+++ ArtConnect_Advanced/src/main/webapp/resources/js/member/memberJoin.js
@@ -51,6 +51,11 @@
 				alert("생년월일을 선택해주세요.");
 				return false;
 			}
+			
+			 if($('#emailAuthKey').val() == ""){
+				alert("인증번호를 입력해주세요.");
+				return false;
+			}
 	      	 
 	        if($('#emailAuthKey').val() != emailAuthCd){
 				alert("인증번호가 일치하지 않습니다.");
@@ -60,6 +65,46 @@
 	        // 모든 조건을 통과하면 회원가입 실행
 	        return true;
 	    }
+	    
+	  function selectAddr() {
+	     new daum.Postcode({
+	            oncomplete: function(data) {
+	                // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+
+	                // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
+	                var roadAddr = data.roadAddress; // 도로명 주소 변수
+	                var extraRoadAddr = ''; // 참고 항목 변수
+
+	                // 법정동명이 있을 경우 추가한다. (법정리는 제외)
+	                // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
+	                if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
+	                    extraRoadAddr += data.bname;
+	                }
+	                // 건물명이 있고, 공동주택일 경우 추가한다.
+	                if(data.buildingName !== '' && data.apartment === 'Y'){
+	                   extraRoadAddr += (extraRoadAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+	                }
+	                // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
+	                if(extraRoadAddr !== ''){
+	                    extraRoadAddr = ' (' + extraRoadAddr + ')';
+	                }
+
+	                // 우편번호와 주소 정보를 해당 필드에 넣는다.
+	                //document.getElementById('sample4_postcode').value = data.zonecode; //우편번호
+	                document.getElementById("addr").value = roadAddr;
+	                //document.getElementById("sample4_jibunAddress").value = data.jibunAddress; //지번주소
+	                
+	                // 참고항목 문자열이 있을 경우 해당 필드에 넣는다.
+	                /* if(roadAddr !== ''){
+	                    document.getElementById("sample4_extraAddress").value = extraRoadAddr;
+	                } else {
+	                    document.getElementById("sample4_extraAddress").value = '';
+	                } */
+
+	                
+	            }
+	        }).open();
+	}
 
 	  $(function() {
 		  console.log("문서가 준비되었습니다.");
