04f6112f7f6b25decdb79104bfb6fa5330a61957
Yangju1009 <145637278+yangju1009@users.noreply.github.com>
2024-03-07 19:07:10 +0900

    스프링 프레임워크 > 스프링 부트로 마이그레이션 과정 1. pom.xml 수정 2. xml 파일들 java 파일로 마이그레이션 (mybatis-config.xml, security-context.xml  제외)

---
+++ ArtConnect_Advanced/src/main/java/com/multi/artConnect/config/ArtConnectApplication.java
@@ -0,0 +1,13 @@
+package com.multi.artConnect.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtConnectApplication.class, args);
+	}
+
+}
---
+++ ArtConnect_Advanced/src/main/java/com/multi/artConnect/config/MailConfig.java
@@ -0,0 +1,30 @@
+package com.multi.artConnect.config;
+
+import java.util.Properties;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class MailConfig {
+
+	@Bean
+	public JavaMailSender javaMailService() {
+		JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+		javaMailSender.setHost("smtp.gmail.com");
+		javaMailSender.setPort(587);
+
+		javaMailSender.setUsername("your-email@gmail.com");
+		javaMailSender.setPassword("your-password");
+
+		Properties props = javaMailSender.getJavaMailProperties();
+		props.put("mail.transport.protocol", "smtp");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.debug", "true");
+
+		return javaMailSender;
+	}
+}
