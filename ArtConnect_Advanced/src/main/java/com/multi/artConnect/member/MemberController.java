fb70bf2af38118081a5ac2f6a9700970a6f03a6e
Yangju1009 <145637278+yangju1009@users.noreply.github.com>
2024-03-08 19:42:36 +0900

    issue: 이미 로그인한 사용자가 로그인페이지에 접근 할 시 에러뜨도록 설정 에러페이지 작성 필요

--- ArtConnect_Advanced/src/main/java/com/multi/artConnect/member/MemberController.java
+++ ArtConnect_Advanced/src/main/java/com/multi/artConnect/member/MemberController.java
@@ -7,7 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,14 +25,9 @@ public class MemberController {
 
 	// 커스텀 로그인 페이지로 넘어가기
 	@GetMapping("/customLogin")
-	public String showLoginForm(Authentication auth) {
-		if (auth != null && auth.isAuthenticated()) {
-			return "/mypage/mypage.jsp";
-			
-		}
+	public String showLoginForm() {
 		return "/member/customLogin";
 	}
-	
 
 	// 회원가입페이지로 넘어가기
 	@GetMapping("/register")
@@ -44,8 +38,7 @@ public class MemberController {
 	//회원가입 시 사용하는 메서드
 	@RequestMapping("/insert.member")
 	public String insert(MemberVO memberVO, AuthVO authVO, Model model, HttpServletRequest request) {
-		try {
-			
+		try {	
 			System.out.println("회원가입 성공: " + memberVO.toString());
 			System.out.println("권한 부여됨: " + authVO.toString());
 			memberService.insert(memberVO, authVO);
@@ -64,10 +57,8 @@ public class MemberController {
 	@RequestMapping("/admin/allMember")
 	public String findAll(Model model) {
 	    
-		List<MemberVO> memberVOList = memberService.findAll();
-	    
-		model.addAttribute("memberList", memberVOList);
-	    
+		List<MemberVO> memberVOList = memberService.findAll();	    
+		model.addAttribute("memberList", memberVOList);    
 		return "/admin/allMember";
 	}
 
